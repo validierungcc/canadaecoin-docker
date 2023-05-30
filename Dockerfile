9ddd05f6b888cbc312d3a5a4ecd21713f0bac23a
Kevin Funke <funke@validierung.cc>
2023-05-30 12:55:43 +0200

    fixed binary not running

--- Dockerfile
+++ Dockerfile
@@ -41,6 +41,8 @@ ENV DEBIAN_FRONTEND noninteractive
 
 RUN apt-get update && \
     apt-get install -y --no-install-recommends \
+        libdb++-dev \
+        libminiupnpc-dev \
         libboost-all-dev && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/*
--- entrypoint.sh
+++ entrypoint.sh
@@ -21,7 +21,7 @@ echo "Created new configuration at ${ECOIN_CONF}"
 fi
 
 if [ $# -eq 0 ]; then
-  /usr/local/bin/canadaecoind -rpcbind=:4444 -rpcallowip=* -printtoconsole=1
+  /usr/local/bin/canadaecoind -rpcbind=:34330 -rpcallowip=* -printtoconsole=1
 else
   exec "$@"
 fi
